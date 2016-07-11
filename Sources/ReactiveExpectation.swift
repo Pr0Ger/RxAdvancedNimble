85f067a5eda54879825a861c98a311f73c20b813
Sergey Petrov <git@pr0ger.org>
2016-07-11 15:39:59 +0300

    Fix access modifier for extension

--- Sources/ReactiveExpectation.swift
+++ Sources/ReactiveExpectation.swift
@@ -7,20 +7,20 @@ import Nimble
 import RxBlocking
 import RxSwift
 
-extension Expectation where T: ObservableType {
-    func toEmitSequence<U where U: Matcher, U.ValueType == [T.E]>(matcher: U, description: String? = nil) {
+public extension Expectation where T: ObservableType {
+    public func toEmitSequence<U where U: Matcher, U.ValueType == [T.E]>(matcher: U, description: String? = nil) {
         expect { try self.expression.evaluate()?.toBlocking().toArray() }.to(matcher, description: description)
     }
 
-    func toEmitFirst<U where U: Matcher, U.ValueType == T.E>(matcher: U, description: String? = nil) {
+    public func toEmitFirst<U where U: Matcher, U.ValueType == T.E>(matcher: U, description: String? = nil) {
         expect { try self.expression.evaluate()?.toBlocking().first() }.to(matcher, description: description)
     }
 
-    func toEmitLast<U where U: Matcher, U.ValueType == T.E>(matcher: U, description: String? = nil) {
+    public func toEmitLast<U where U: Matcher, U.ValueType == T.E>(matcher: U, description: String? = nil) {
         expect { try self.expression.evaluate()?.toBlocking().last() }.to(matcher, description: description)
     }
 
-    func toEmitSingle<U where U: Matcher, U.ValueType == T.E>(matcher: U, description: String? = nil) {
+    public func toEmitSingle<U where U: Matcher, U.ValueType == T.E>(matcher: U, description: String? = nil) {
         expect { try self.expression.evaluate()?.toBlocking().single() }.to(matcher, description: description)
     }
 }
